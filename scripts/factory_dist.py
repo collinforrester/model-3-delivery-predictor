9b451cf3a87e385c8c48b3ca72c2140c51e04084
Jake Cloyd <dyolcekaj@gmail.com>
2020-01-17 08:50:43 -0600

    Updates to factory distance script

--- scripts/factory_dist.py
+++ scripts/factory_dist.py
@@ -11,28 +11,58 @@ tesla_lat = radians(37.493530)
 tesla_lon = radians(-121.942162)
 
 url = "https://nominatim.openstreetmap.org/search?format=json&state=%s&city=%s"
+state_url = "https://nominatim.openstreetmap.org/search?format=json&state=%s"
 
 
 def get_factory_dist(state, city):
-    q = url % (state, city)
+    lat, lon = -1, -1
+    if type(state) != str or not state:
+        return -1
+
+    if type(city) != str or not city:
+        lat, lon = get_state_center(state)
+    else:
+        lat, lon = get_city_center(state, city)
+
+        if lat == -1 or lon == -1:
+            lat, lon = get_state_center(state)
+
+    if lat == -1 or lon == -1:
+        return -2
+
+    dlat = tesla_lat - lat
+    dlon = tesla_lon - lon
+
+    a = sin(dlat / 2) ** 2 + cos(tesla_lat) * cos(lat) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return int(radius * c)
+
+
+def get_city_center(state, city):
+    q = url % (urllib.parse.quote(state), urllib.parse.quote(city))
 
     data = urllib.request.urlopen(q).read()
     j = json.loads(data.decode('utf-8'))
 
     for entry in j:
         if entry['class'] == 'place' and entry['type'] == 'city':
-            lat = radians(float(entry['lat']))
-            lon = radians(float(entry['lon']))
+            return radians(float(entry['lat'])), radians(float(entry['lon']))
 
-            dlat = tesla_lat - lat
-            dlon = tesla_lon - lon
+    return -1, -1
 
-            a = sin(dlat / 2) ** 2 + cos(tesla_lat) * cos(lat) * sin(dlon / 2) ** 2
-            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+def get_state_center(state):
+    q = state_url % (urllib.parse.quote(state))
 
-            return int(radius * c)
+    data = urllib.request.urlopen(q).read()
+    j = json.loads(data.decode('utf-8'))
 
-    raise Exception("didn't find your city")
+    for entry in j:
+        if entry["class"] == "boundary" and "United States of America" in entry["display_name"]:
+            return radians(float(entry['lat'])), radians(float(entry['lon']))
+
+    return -1, -1
+
 
 
 def main():
